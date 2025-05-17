3e3c590661250e0f41e13e2913c433a674a7f1de
Tarek Magdy <tarek.magdy1245@gmail.com>
2025-05-17 23:17:32 +0300

    Update cai_code.ino

--- cai_code/cai_code.ino
+++ cai_code/cai_code.ino
@@ -1,58 +1,55 @@
 #include <Servo.h>
 
-Servo base;    // القاعدة
-Servo arm;     // الذراع
-Servo gripper; // الجريبر
+Servo base;    
+Servo arm;     
+Servo gripper;
 
-String command = ""; // لتخزين الأوامر المستلمة
+String command = ""; 
 
 void setup() {
-  Serial.begin(9600); // بدء الاتصال بالسيريال
-  base.attach(9);     // توصيل السيرفو الخاص بالقاعدة بالمنفذ 9
-  arm.attach(10);     // توصيل السيرفو الخاص بالذراع بالمنفذ 10
-  gripper.attach(11); // توصيل السيرفو الخاص بالجريبر بالمنفذ 11
+  Serial.begin(9600); 
+  base.attach(9);     
+  arm.attach(10);    
+  gripper.attach(11);
 
-  // الوضع الابتدائي
-  arm.write(90);    // الذراع للأعلى
-  gripper.write(0); // الجريبر مغلق
-  base.write(90);   // القاعدة في المنتصف
+  arm.write(90);    
+  gripper.write(0); 
+  base.write(90);  
 }
 
 void loop() {
-  // قراءة الأوامر من السيريال
+  
   while (Serial.available()) {
     char c = Serial.read();
-    if (c == '\n') { // عند استقبال السطر بالكامل
-      handleCommand(command); // تنفيذ الأمر
-      command = "";           // إعادة تعيين النص
+    if (c == '\n') { 
+      handleCommand(command); 
+      command = "";         
     } else {
-      command += c; // إضافة الحروف إلى النص
+      command += c; 
     }
   }
 }
 
 void handleCommand(String cmd) {
   if (cmd == "RED") {
-    // اللون الأحمر
-    base.write(0);      // القاعدة إلى 0°
-    arm.write(0);       // الذراع للأسفل
-    gripper.write(180); // الجريبر مفتوح
+    base.write(0);     
+    arm.write(0);       
+    gripper.write(180); 
     delay(1000);
   } else if (cmd == "YELLOW") {
     // اللون الأصفر
-    base.write(180);    // القاعدة إلى 180°
-    arm.write(0);       // الذراع للأسفل
-    gripper.write(180); // الجريبر مفتوح
+    base.write(180);   
+    arm.write(0);      
+    gripper.write(180); 
     delay(1000);
   }else if (cmd == "PURPLE") {
-    // اللون الأصفر
-    base.write(135);    // القاعدة إلى 180°
-    arm.write(0);       // الذراع للأسفل
-    gripper.write(180); // الجريبر مفتوح
+    base.write(135);   
+    arm.write(0);       
+    gripper.write(180); 
     delay(1000);
   }
   
   else {
-    Serial.println("Unknown command"); // طباعة رسالة إذا كان الأمر غير معروف
+    Serial.println("Unknown command"); 
   }
 }
